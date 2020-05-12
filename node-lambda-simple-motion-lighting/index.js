7879eea1402b282570776c1f701ea82539f87ec7
Bob Florian <bob@smartthings.com>
2020-05-12 18:58:58 -0400

    Motion lighting serverless fix

--- node-lambda-simple-motion-lighting/index.js
+++ node-lambda-simple-motion-lighting/index.js
@@ -1,5 +1,5 @@
 const smartApp = require('./smartapp')
 
 module.exports.handle = (event, context, callback) => {
-    app.handleLambdaCallback(event, context, callback);
+    smartApp.handleLambdaCallback(event, context, callback);
 };
